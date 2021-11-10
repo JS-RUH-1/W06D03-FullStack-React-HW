ca58ca1df24d0787195496f5b0187fb3bf608f5e
Shahad-F <shahadbinsweleh@gmail.com>
2021-11-10 20:17:26 +0300

    FrondEnd and BackEnd

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,25 +1,105 @@
 const express =require('express');
 const cors=require('cors');
-const {posts}=require('./posts.js');
+const  posts =require('./posts.json');
 const app=express();
 const PORT =3001;
+const fs=require('fs')
 
-let currentId = posts.length;
+
+ 
 
 app.use(cors({origin:'http://localhost:3001'}))
+
 app.use(express.json());
 
+
+
 app.get('/posts',(req,res)=>{
-    console.log(posts)
+    // console.log(posts)
     res.json(posts);
 })
 
+//POST
 app.post('/posts',(req,res)=>{
-    const post =req.body;
-    post.id = ++currentId;
-    posts.push(post);
-    res.json(post);
+
+    const newPost= {
+        id:  posts.length + 1,
+        title:req.body.title,
+        body:req.body.body,
+        date:req.body.date
+    }
+     posts.push(newPost)
+    console.log(req.body)
+    
+    fs.writeFile('./posts.json',JSON.stringify(posts,null,2),(err)=>{
+        if(err) throw err;
+    })
+
+    res.json(posts)  
 })
+
+
+//DELETE
+
+app.delete("/posts/:id",(req,res)=>{
+
+    console.log("id from back"+req.params.id)
+
+const found = posts.find((item)=>{
+    return item.id === parseInt(req.params.id)
+})
+
+if(found){
+    const targetIndex = posts.indexOf(found)
+    posts.splice(targetIndex,1)
+    // res.send(posts)
+}
+else{
+    app.sendStatus(404)
+}
+fs.writeFile('./posts.json',JSON.stringify(posts,null,2),(err)=>{
+
+    console.log(posts)
+    if(err) throw err;
+    res.send(posts)
+})
+})
+
+// PUT----> Update
+
+app.put('/posts/:id',(req,res)=>{
+
+    const found = posts.find((item)=>{
+
+        return item.id === parseInt(req.params.id)
+    })
+
+    if (found){
+        const update ={
+
+            id:found.id,
+            title:req.body.title,
+            body:req.body.body,
+            date:req.body.date
+        }
+
+        const targetIndex = posts.indexOf(found)
+        posts.splice(targetIndex,1,update)
+        res.send(posts)
+
+
+    }
+    else{
+        app.sendStatus(404)
+    }
+})
+
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log('connected on= http://localhost:3001/posts')
--- frontEnd/blog-react/src/App.js
+++ frontEnd/blog-react/src/App.js
@@ -1,51 +1,130 @@
  import axios from 'axios';
  import { useState ,useEffect } from 'react';
- 
-import './App.css';
+ import './App.css';
 
 function App() {
 
-  const [post,setPost] =useState({title:'Shahad'})
-  const [posts,setPosts]= useState([])
- 
+  const [post,setPost] = useState({id:'##',title:'',body:'', date:'2/2/2020'})
 
+   
+  const [addtitle,setTitle]= useState('')
+  const [addbody,setBody]= useState('')
+  const [adddate,setDate]= useState('') 
+
+
+  const [posts,setPosts]= useState([])
+   
+
+/// get all data
+//////////GET////////////////
   useEffect(()=>{
 
-    axios.get('http://localhost:3001/posts')
-    .then((res)=>{
+      axios.get('http://localhost:3001/posts')
+     .then((res)=>{
       console.log(res.data)
       setPosts(res.data)
 
     })
   },[])
 
-
+//////////POST ////////////////
   const handleClick=()=>{
-    axios.post('http://localhost:3001/posts',post)
+
+    axios.post('http://localhost:3001/posts',{title:addtitle ,body:addbody,date:adddate})
+
     .then((res)=>{
       console.log(res.data)
-      setPosts([...posts,res.data])
+      setPosts(res.data)
+      setTitle('')
+      setBody('')
+      setDate('')
     })
+
   }
+///////DELETE///////////////////
+  const handelDelete =(id)=>{
+       console.log(id)
+     
+       axios.delete(`http://localhost:3001/posts/${id}`)
+      .then((res)=>{
+
+        console.log(res.data)
+        setPosts(res.data)
+        
+      })}
+
+      //////////////PUT//////////////
+      const handelUpdate=(id)=>{
+        console.log(id)
+        axios.put(`http://localhost:3001/posts/${id}`,{title:addtitle,body:addbody,date:adddate})
+        .then((res)=>{
+          setPosts(res.data)
+
+        })
+
+      }
+
   return (
      <>
-<div className='App'>
+  <div className='App'>
+
   <header className='App-header'>
 
-    {
-      posts.map((post)=>{
-        return <li>ID :{post.id} title: {post.title} Body: {post.body}</li>
-      })
-    }
+    {posts.map((post)=>{
+     <br/>
+        return <li>
 
-    <button onClick={handleClick}>POST</button>
+    ID:{post.id} title: {post.title} Body: {post.body} Date:{post.date} 
+     <button onClick={()=>handelDelete(post.id)}>Delete</button>
+     <button onClick={()=>handelUpdate(post.id)} >Updait</button><br/>
+           
+           
+           
+             </li> 
+       
+       })}
+
+
+
+
+  <form>
+
+  <input 
+   type='text'
+   placeholder='title' 
+   value={addtitle}
+   onChange ={ e=> setTitle(e.target.value)}></input><br/>
+
+  <input 
+  type='text' 
+  placeholder='body'
+  value= {addbody}
+  onChange ={ e=> setBody(e.target.value)}></input><br/>
+
+<input 
+  type='text' 
+  placeholder='date'
+  value= {adddate}
+  onChange ={ e=> setDate(e.target.value)}></input><br/>
+
+
+
+
+  </form>
+<br/>
+
+   {/* <button onClick={handelBtn}>click me </button> */}
+
+    <button onClick={handleClick}>POST</button><br/>
+     
+    
+     
 
   </header>
 
 
-</div>
-
-     </>
+   </div>
+</>
   );
 }
 
